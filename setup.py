ca58816b53d3b9e6f9cd4c68b8f9a60821a654fd
Prithish <tbiju@users.noreply.github.com>
2026-05-26 21:42:21 +0530

    chore: bump version to 2.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='biju-cli',
-    version='2.0.3',
+    version='2.0.4',
     description='An advanced AI CLI + TUI developed by Prithish',
     author='Prithish',
     packages=find_packages(),
